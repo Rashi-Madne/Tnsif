38c3367b344728cd0d47e143f438f96fb154130f
Rashi <rashimadne13@gmail.com>
2026-04-09 08:08:18 +0530

    commit

--- src/com/learnjava/VariablesDemo.java
+++ src/com/learnjava/VariablesDemo.java
@@ -2,4 +2,18 @@ package com.learnjava;
 
 public class VariablesDemo {
 
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Rahul";
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isStudent);
+        System.out.println(name);
+    }
 }
